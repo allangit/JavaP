6a728e6db875a39d29f8bd339ec10b65006043a4
descartes640@gmail.com <descartes640@gmail.com>
2023-07-22 08:10:45 -0600

    Practica Grafos

--- Grafos.java
+++ Grafos.java
@@ -1,6 +1,15 @@
 
+/*
+    practica de grafos construir matriz de adyacencia 
+    2.construir métodos verificar arista
+    3.construir métodos borrar arista
+    4.contruir método obtener vecinos
+    5.validación de los metodos unitTest
+
+*/
 package com.mycompany.principal;
 
 public class Grafos {
+    
     
 }
--- Principal.java
+++ Principal.java
@@ -4,6 +4,6 @@ package com.mycompany.principal;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
     }
 }
